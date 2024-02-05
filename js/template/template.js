3a2ba7fc8ea5a3021ae47b2c15b8676d8d5c29b0
Muhammad Faisal A <sidiqfaisal30@gmail.com>
2024-02-05 10:58:21 +0700

    updatess

--- js/template/template.js
+++ js/template/template.js
@@ -1,16 +1,18 @@
-export let URLGeoJson = "https://asia-southeast2-project3-403614.cloudfunctions.net/getDataGis";
-export let URLPostGCF = "https://asia-southeast2-project3-403614.cloudfunctions.net/insertDataGis";
-export let tableTag="tr";
-export let tableRowClass="content is-small";
-export let tableTemplate=`
+export let URLGeoJson =
+  "https://asia-southeast2-project3-403614.cloudfunctions.net/getDataGis";
+export let urlPOSTGCF =
+  "https://asia-southeast2-project3-403614.cloudfunctions.net/insertDataGis";
+export let tableTag = "tr";
+export let tableRowClass = "content is-small";
+export let tableTemplate = `
 <td>#NAME#</td>
 <td >#KORDINAT#</td>
 <td>#TYPE#</td>
-`
+`;
 export const clickpopup = `
 Long : #LONG#<br>
 Lat  : #LAT#<br>
 X   : #X#<br>
 Y   : #Y#<br>
 HDMS : #HDMS#<br>
-`
+`;
